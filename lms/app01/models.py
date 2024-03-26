626ed43c76370501b70e1b61b8f2dda5e38714aa
Yingl37 <353526082@qq.com>
2024-03-26 16:32:13 +0800

    跑通版本

--- lms/app01/models.py
+++ lms/app01/models.py
@@ -13,7 +13,7 @@ class Publisher(models.Model):
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
-    publisher = models.ForeignKey(to=Publisher)  # Django中创建外键联表操作
+    publisher = models.ForeignKey(to=Publisher, on_delete=models.CASCADE)  # 在django2.0后，定义外键和一对一关系的时候需要加on_delete选项，此参数为了避免两个表里的数据不一致问题，
 
 
 # 作者的类
@@ -21,6 +21,6 @@ class Author(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
     # 一个作者可以对应多本书，一本书也可以有多个作者，多对多，在数据库中创建第三张表
-    book = models.ManyToManyField(to=Book)
+    # book = models.ManyToManyField(to=Book)
 
 
--- lms/lms/urls.py
+++ lms/lms/urls.py
@@ -13,25 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from app01 import views
 
 urlpatterns = [
     # 管理员账户登陆
-    url(r'^admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
     # 出版社列表
-    url(r'^publisher_list/', views.publisher_list),
-    url(r'^add_publisher/', views.add_publisher),
-    url(r'^drop_publisher/', views.drop_publisher),
-    url(r'^edit_publisher/', views.edit_publisher),
-    url(r'^book_list/', views.book_list),
-    url(r'^add_book/', views.add_book),
-    url(r'^drop_book/', views.drop_book),
-    url(r'^edit_book/', views.edit_book),
-    url(r'^author_list/', views.author_list),
-    url(r'^add_author/', views.add_author),
-    url(r'^drop_author/', views.drop_author),
-    url(r'^edit_author/', views.edit_author),
-    url(r'^$', views.publisher_list),
+    re_path(r'^publisher_list/', views.publisher_list),
+    re_path(r'^add_publisher/', views.add_publisher),
+    re_path(r'^drop_publisher/', views.drop_publisher),
+    re_path(r'^edit_publisher/', views.edit_publisher),
+    re_path(r'^book_list/', views.book_list),
+    re_path(r'^add_book/', views.add_book),
+    re_path(r'^drop_book/', views.drop_book),
+    re_path(r'^edit_book/', views.edit_book),
+    re_path(r'^author_list/', views.author_list),
+    re_path(r'^add_author/', views.add_author),
+    re_path(r'^drop_author/', views.drop_author),
+    re_path(r'^edit_author/', views.edit_author),
+    re_path(r'^$', views.publisher_list),
 ]
